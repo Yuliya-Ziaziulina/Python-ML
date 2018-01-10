95bdfb1fa844fc98228a1f62f2e20d33c45621c0
Yuliya_Ziaziulina <yuliya.ziaziulina@gmail.com>
2018-01-10 22:42:49 +0300

    adaline class added + test updated

--- TestAdal.py
+++ TestAdal.py
@@ -4,13 +4,35 @@ df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris
 df.tail()
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X,y, classifier, resolution=0.02):
+    markers = ('s', 'x','o', '^','v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    #plot the decision surface
+
+    x1_min, x1_max = X[:,0].min() - 1, X[:,0].max() +1
+    x2_min, x2_max = X[:,1].min() - 1, X[:,1].max() +1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max,resolution), np.arange(x2_min,x2_max,resolution))
+
+
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1,xx2,Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
 
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
 
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+"""fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
 ada1 = Adaline.AdalineGD(n_iter=10, eta=0.01).fit(X, y)
 ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
 ax[0].set_xlabel('Epochs')
@@ -22,5 +44,29 @@ ax[1].plot(range(1, len(ada2.cost_) + 1),ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-squared-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()"""
+
+
+"""standartization"""
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0]-X[:,0].mean())/X[:,0].std()
+X_std[:,1] = (X[:,1]-X[:,1].mean())/X[:,1].std()
+
+ada = Adaline.AdalineGD(n_iter=15, eta=0.01).fit(X_std, y)
+plot_decision_regions(X_std,y,classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('patel length [standardized]')
+plt.legend(loc='upper left')
 plt.show()
+
+plt.plot(range(1, len(ada.cost_)+1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
+
+
+
+
 
